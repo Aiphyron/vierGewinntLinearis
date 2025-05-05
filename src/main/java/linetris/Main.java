2833640685aaceefb029c6504d8c7b6d4b872404
casuallymax <terrierzockt@gmail.com>
2025-05-05 16:13:23 +0200

    :lipstick: added look & feel dependency

--- src/main/java/linetris/Main.java
+++ src/main/java/linetris/Main.java
@@ -1,7 +1,23 @@
 package linetris;
 
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        try {
+            UIManager.setLookAndFeel( new FlatLightLaf() );
+        } catch( Exception ex ) {
+            System.err.println( "Failed to initialize LaF" );
+        }
+
+        Frame f = new Frame();
+
+        f.setSize(500, 500);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
