fdb5393234eb8869421e3b401b9df7ab54f38088
Aiphyron <mjoekel@t-online.de>
2025-05-29 16:51:01 +0200

    :sparkles: added first functionality

--- src/main/java/ui/model/GameModel.java
+++ src/main/java/ui/model/GameModel.java
@@ -4,8 +4,19 @@ import ui.model.dimensions.BoardDimensions;
 
 public class GameModel {
 
+    // PlayerEnum [Row][Column]
     private PlayerEnum[][] board;
 
+    // Player names and client names
+    private String player1Name;
+    private String client1Name;
+    private String player2Name;
+    private String client2Name;
+
+    // Save which player this is
+    private PlayerEnum playerIdentity = PlayerEnum.ONE;
+
+    // Dimensions of the board
     private final BoardDimensions boardDimensions;
 
     public GameModel() {
@@ -15,6 +26,37 @@ public class GameModel {
 
     public PlayerEnum[][] getBoard() {
         return board;
+    }
+
+    public void setBoardPiece(int col) {
+        if (col >= 0 && col < boardDimensions.getCols()) {
+            int row = getAvailableRow(col);
+            if (row == -1) {
+                throw new IllegalArgumentException("Column is full");
+            }
+            if (this.board[row][col] != null) {
+                throw new IllegalArgumentException("Cell already occupied");
+            }
+            this.board[row][col] = playerIdentity;
+        } else {
+            throw new IndexOutOfBoundsException("Row or column out of bounds");
+        }
+    }
+
+    /**
+     * Returns the row index of the first available cell in the specified column.
+     * If no cells are available, returns -1.
+     *
+     * @param col the column index to check
+     * @return the row index of the first available cell, or -1 if none are available
+     */
+    private int getAvailableRow(int col) {
+        for (int row = boardDimensions.getRows() - 1; row >= 0; row--) {
+            if (this.board[row][col] == null) {
+                return row;
+            }
+        }
+        return -1;
     }
 
     public BoardDimensions getBoardDimensions() {
@@ -43,4 +85,17 @@ public class GameModel {
 
     }
 
+    public void setBoard(PlayerEnum[][] board) {
+        if (board.length != boardDimensions.getRows() || board[0].length != boardDimensions.getCols()) {
+            throw new IllegalArgumentException("Board dimensions do not match the model's dimensions");
+        }
+        this.board = board;
+    }
+
+    public void setPlayerIdentity(PlayerEnum playerEnum) {
+        if (playerEnum != PlayerEnum.ONE && playerEnum != PlayerEnum.TWO) {
+            throw new IllegalArgumentException("Invalid player identity");
+        }
+        this.playerIdentity = playerEnum;
+    }
 }
--- src/main/java/ui/view/BoardPanel.java
+++ src/main/java/ui/view/BoardPanel.java
@@ -1,17 +1,30 @@
 package ui.view;
 
 import ui.model.GameModel;
+import ui.model.PlayerEnum;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class BoardPanel extends JPanel {
 
     private final GameModel model;
 
+    private static Color PLAYER1_COLOR = new Color(255, 0, 0); // Red
+    private static Color PLAYER2_COLOR = new Color(255, 255, 0); // Yellow
+
     public BoardPanel(ConnectFourFrame view, GameModel model) {
         this.model = model;
         this.setPreferredSize(model.getBoardDimensions().getOuterDimension());
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                handleBoardClick(e.getX(), e.getY());
+            }
+        });
     }
 
     @Override
@@ -32,6 +45,8 @@ public class BoardPanel extends JPanel {
 
     private void drawBoard(Graphics2D g2d) {
         int margin = this.model.getBoardDimensions().getMargin();
+        PlayerEnum[][] boardState = this.model.getBoard();
+
         int cursorX = margin;
         int cursorY = margin;
 
@@ -50,6 +65,16 @@ public class BoardPanel extends JPanel {
         int pieceHeight = this.model.getBoardDimensions().getPieceDimensions().getDimension().height;
         for (int col = 0; col < this.model.getBoardDimensions().getCols(); col++) {
             for (int row = 0; row < this.model.getBoardDimensions().getRows(); row++) {
+                PlayerEnum player = boardState[row][col];
+
+                if (player == null) {
+                    g2d.setColor(Color.WHITE);
+                } else if (player == PlayerEnum.ONE) {
+                    g2d.setColor(PLAYER1_COLOR);
+                } else if (player == PlayerEnum.TWO) {
+                    g2d.setColor(PLAYER2_COLOR);
+                }
+
                 g2d.fillOval(
                         cursorX + pieceMargin,
                         cursorY + pieceMargin,
@@ -60,6 +85,77 @@ public class BoardPanel extends JPanel {
             cursorY = margin;
             cursorX += pieceWidth;
         }
+    }
+
+    private void handleBoardClick(int x, int y) {
+        int margin = this.model.getBoardDimensions().getMargin();
+        int pieceWidth = this.model.getBoardDimensions().getPieceDimensions().getDimension().width;
+        int pieceHeight = this.model.getBoardDimensions().getPieceDimensions().getDimension().height;
+
+        int col = (x - margin) / pieceWidth;
+        int row = (y - margin) / pieceHeight;
+
+        if (col >= 0 && col < this.model.getBoardDimensions().getCols() &&
+                row >= 0 && row < this.model.getBoardDimensions().getRows()) {
+            // Do something with the clicked cell, e.g.:
+            System.out.println("Clicked cell: (" + col + ", " + row + ")");
+            // Set the piece for the current player
+            this.model.setBoardPiece(col);
+
+            // Switch player identity for the next turn, HAS ERROR
+            if (this.model.getBoard()[row][col] == PlayerEnum.ONE) {
+                this.model.setPlayerIdentity(PlayerEnum.TWO);
+            } else {
+                this.model.setPlayerIdentity(PlayerEnum.ONE);
+            }
+            // Repaint the board to reflect the change
+            repaint();
+        }
+
+        if (checkBottomRowFilled()) {
+            clearBottomRow();
+        }
+    }
+
+    private boolean checkBottomRowFilled() {
+        PlayerEnum[][] board = this.model.getBoard();
+        for (int col = 0; col < this.model.getBoardDimensions().getCols(); col++) {
+            if (board[this.model.getBoardDimensions().getRows() - 1][col] == null) {
+                return false; // If any cell in the bottom row is empty, return false
+            }
+        }
+        return true;
+    }
+
+    private void clearBottomRow() {
+        PlayerEnum[][] board = this.model.getBoard();
+        for (int col = 0; col < this.model.getBoardDimensions().getCols(); col++) {
+            board[this.model.getBoardDimensions().getRows() - 1][col] = null;
+        }
+
+        for(int col = 0; col < this.model.getBoardDimensions().getCols(); col++) {
+            for(int row = this.model.getBoardDimensions().getRows() - 2; row >= 0; row--) {
+                board[row + 1][col] = board[row][col]; // Shift pieces down
+                board[row][col] = null; // Clear the top cell
+            }
+        }
+
+        this.model.setBoard(board);
+        repaint();
+    }
+
+    private PlayerEnum[][] createTestBoard() {
+        PlayerEnum[][] testBoardState = new PlayerEnum[8][8];
+        testBoardState[0][7] = PlayerEnum.ONE;
+        testBoardState[1][1] = PlayerEnum.TWO;
+        testBoardState[2][2] = PlayerEnum.ONE;
+        testBoardState[3][3] = PlayerEnum.TWO;
+        testBoardState[4][4] = PlayerEnum.ONE;
+        testBoardState[5][5] = PlayerEnum.TWO;
+        testBoardState[6][6] = PlayerEnum.ONE;
+        testBoardState[7][7] = PlayerEnum.TWO;
+
+        return testBoardState;
     }
 
 }
